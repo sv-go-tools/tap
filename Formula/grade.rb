42e5dbed1e62b4aa94cf2c41c90654c005262519
goreleaserbot <goreleaser@carlosbecker.com>
2020-01-22 22:44:23 -0600

    Brew formula update for grade version v1.1.1

--- Formula/grade.rb
+++ Formula/grade.rb
@@ -2,16 +2,16 @@
 class Grade < Formula
   desc "grade transforms Go benchmark data into various format so that you can uploads the data to a database and track the performance over time."
   homepage "https://github.com/sv-tools/grade"
-  version "1.1.0"
+  version "1.1.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/sv-tools/grade/releases/download/v1.1.0/grade-v1.1.0-darwin-amd64.tar.gz"
-    sha256 "4ec2f72d24998375f29a9dd83252a79dce295a426a743c7ac6dec3090273ed92"
+    url "https://github.com/sv-tools/grade/releases/download/v1.1.1/grade-v1.1.1-darwin-amd64.tar.gz"
+    sha256 "2f30936f16f81590b70202dced96f69e342cebfdb01fe1e0a7304210bd0abf0d"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/sv-tools/grade/releases/download/v1.1.0/grade-v1.1.0-linux-amd64.tar.gz"
-      sha256 "8452e1b415ef0a71ff50e997c5831f94c1790aa9001ccdcc60a408e44f3168de"
+      url "https://github.com/sv-tools/grade/releases/download/v1.1.1/grade-v1.1.1-linux-amd64.tar.gz"
+      sha256 "3987764843d5b0ec2ea499ad436507f3c283135838c95999dc3b4f9f7638faa9"
     end
   end
 
